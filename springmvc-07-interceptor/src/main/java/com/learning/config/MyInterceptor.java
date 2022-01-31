3035783cccdf41ff45fde921947b2ef2388883f2
yuechengu <gyc822@qq.com>
2022-02-01 00:05:05 +0800

    1/31 night

---
+++ springmvc-07-interceptor/src/main/java/com/learning/config/MyInterceptor.java
@@ -0,0 +1,28 @@
+package com.learning.config;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyInterceptor implements HandlerInterceptor {
+
+    //在请求处理的方法之前执行
+    //如果返回true执行下一个拦截器
+    //如果返回false就不执行下一个拦截器
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
